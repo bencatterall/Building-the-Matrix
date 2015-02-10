bc7bb9f32f5e633c12eab68848b023d32e59dfc8
dangercrow <dangercrow@gmail.com>
2015-02-10 12:25:42 +0000

    Fixed graphics bug
    Ben fixed his bug on my machine

--- Building-the-Matrix/src/Texture.cpp
+++ Building-the-Matrix/src/Texture.cpp
@@ -5,6 +5,21 @@ Texture::Texture(std::string filename) {
 	unsigned int width, height;
 	std::vector<unsigned char> image = imgLoader.loadImage(filename, width, height);
 
+	//handle no image
+	if (image.size() == 0) {
+		glGenTextures(1, &textureID);
+		glBindTexture(GL_TEXTURE_2D, textureID);
+
+		this->width = 0;
+		this->height = 0;
+		pixelDataSize = 0;
+		pixelData = new unsigned char[pixelDataSize];
+
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, 0, 0, 0, GL_RGBA, GL_UNSIGNED_BYTE, pixelData);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+		return;
+	}
 	//Copy data across
 	//TODO: Stop all this copying
 	this->width = width;
