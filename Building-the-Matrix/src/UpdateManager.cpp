c12e3ce6fc428b935f396653874ba28213b0bcb5
bencatterall <ben_catterall@hotmail.co.uk>
2015-02-19 22:40:31 +0000

    Adding MessageTypes

--- Building-the-Matrix/src/UpdateManager.cpp
+++ Building-the-Matrix/src/UpdateManager.cpp
@@ -1,17 +1,28 @@
 #include "UpdateManager.hpp"
+#include "Serializer.hpp"
 
-std::vector<char> buildLoginMessage() {
+#include <type_traits> //for std::underlying_type
 
+std::vector<char> UpdateManager::buildLoginMessage() {
+	MessageType type = MessageType::LOGIN;
+	int value = static_cast<std::underlying_type<MessageType>::type>(type);
+
+	std::vector<char> data;
+	return data;
 }
 
-std::vector<char> buildLogoutMessage() {
-
+std::vector<char> UpdateManager::buildLogoutMessage() {
+	std::vector<char> data;
+	return data;
 }
 
 std::vector<char> buildStateChangeMessage() {
-
+	std::vector<char> data;
+	return data;
 }
 
-std::vector<char> buildMessage(){
 
+std::vector<char> buildObjectDeletedMessage() {
+	std::vector<char> data;
+	return data;
 }
--- Building-the-Matrix/src/UpdateManager.hpp
+++ Building-the-Matrix/src/UpdateManager.hpp
@@ -4,20 +4,30 @@
 #include "Serializer.hpp"
 
 #include <vector>
+
+///
+///	MESSAGE FORMAT:
+///   Message type: int ( 4 bytes)
+///   Messsage length : int (4 bytes)
+///	  Message data follows
+///   
+/// All are in Network byte order
+///
+///
+///
 class UpdateManager {
 
 
 
+
 public:
-	enum MessageType {
-
+	enum class MessageType {
+		LOGIN=1, LOGOUT, STATE_CHANGED, OBJECT_DELETED
 	};
-
-
 
 	std::vector<char> buildLoginMessage();
 	std::vector<char> buildLogoutMessage();
 	std::vector<char> buildStateChangeMessage();
-	std::vector<char> buildMessage();
+	std::vector<char> buildObjectDeletedMessage();
 };
 #endif
