acf35d6ff4f841d832c9fe91d5ff332f30addbeb
bencatterall <ben_catterall@hotmail.co.uk>
2015-02-19 22:21:46 +0000

    Adding definitions

--- Building-the-Matrix/src/UpdateManager.cpp
+++ Building-the-Matrix/src/UpdateManager.cpp
@@ -1,1 +1,17 @@
 #include "UpdateManager.hpp"
+
+std::vector<char> buildLoginMessage() {
+
+}
+
+std::vector<char> buildLogoutMessage() {
+
+}
+
+std::vector<char> buildStateChangeMessage() {
+
+}
+
+std::vector<char> buildMessage(){
+
+}
