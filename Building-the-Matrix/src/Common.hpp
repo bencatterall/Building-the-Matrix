2253d8ba7c2c3100e55cbec4e5f0b89aa5ce653e
Daniel Wong <wonglkd@gmail.com>
2015-02-20 16:02:31 +0000

    Move compile-time platform detection into Common.hpp

--- Building-the-Matrix/src/Common.hpp
+++ Building-the-Matrix/src/Common.hpp
@@ -1,6 +1,19 @@
 #ifndef COMMON_H
 #define COMMON_H
 
+#define PLATFORM_WINDOWS  1
+#define PLATFORM_MAC      2
+#define PLATFORM_UNIX     3
+
+#if defined(_WIN32)
+#define PLATFORM PLATFORM_WINDOWS
+#elif defined(__APPLE__)
+#define PLATFORM PLATFORM_MAC
+#else
+#define PLATFORM PLATFORM_UNIX
+#endif
+
+#if PLATFORM == PLATFORM_WINDOWS
 #include <Windows.h>
 #include "..\Dependencies\glew\glew.h"
 #include "..\Dependencies\GLFW\glfw3.h"
